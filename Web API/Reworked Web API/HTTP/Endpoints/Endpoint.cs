e64fa433057dd710647b10fa68d3918ad91603f0
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-10-12 23:15:56 +0200

    Added new endpoint class and custom class attribute

---
+++ Web API/Reworked Web API/HTTP/Endpoints/Endpoint.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace API.HTTP.Endpoints
+{
+	/// <summary>
+	/// An abstract class representing a webserver endpoint that can be invoked.
+	/// </summary>
+	public abstract class Endpoint
+	{
+		/// <summary>
+		/// The request object passed to this <see cref="Endpoint"/> instance.
+		/// </summary>
+		protected HttpListenerRequest Request { get; }
+		/// <summary>
+		/// The response object passed to this <see cref="Endpoint"/> instance.
+		/// </summary>
+		protected HttpListenerResponse Response { get; }
+
+		/// <summary>
+		/// Initializes a new instance of <see cref="Endpoint"/>.
+		/// </summary>
+		/// <param name="request">The <see cref="HttpListenerRequest"/> object to pass to this endpoint.</param>
+		/// <param name="response">The <see cref="HttpListenerResponse"/> object to pass to this endpoint.</param>
+		public Endpoint(HttpListenerRequest request, HttpListenerResponse response)
+		{
+			Request = request;
+			Response = response;
+		}
+
+		/// <summary>
+		/// Invokes this <see cref="Endpoint"/>.
+		/// </summary>
+		public abstract void Invoke();
+
+		/// <summary>
+		/// Splits a url query into a dictionary.
+		/// </summary>
+		protected static Dictionary<string, string> SplitQuery(string query)
+		{
+			var queryParts = query.Split('?');
+			// Return empty dict if there is no query string
+			if (queryParts.Length == 1) return new Dictionary<string, string>();
+
+			// Split the query string into key-value pairs
+			var items = Uri.UnescapeDataString(queryParts[1]).Split('&');
+			var outDict = new Dictionary<string, string>();
+			foreach (var item in items)
+			{
+				// Try to parse every key-value pair
+				var keyValuePair = item.Split('=', 2);
+				outDict[keyValuePair[0]] = string.Join("", keyValuePair[1..]);
+			}
+			return outDict;
+		}
+	}
+}
---
+++ Web API/Reworked Web API/HTTP/Endpoints/JsonEndpoint.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace API.HTTP.Endpoints
+{
+	/// <summary>
+	/// An abstract subclass of <see cref="Endpoint"/>. This contains functions specifically made to handle a JSON type request.
+	/// </summary>
+	/// <remarks>
+	/// To make actual JSON endpoints you must extend this class and implement the abstract classes. At least one of these functions
+	/// must write data to the response object. If nothing is sent, a 404 will be sent instead.
+	/// Attributes must be used to specify the target url.
+	/// </remarks>
+	public abstract class JsonEndpoint : Endpoint
+	{
+		/// <summary>
+		/// Initializes a new instance of <see cref="JsonEndpoint"/>.
+		/// </summary>
+		/// <param name="request">The <see cref="HttpListenerRequest"/> object to pass to this endpoint.</param>
+		/// <param name="response">The <see cref="HttpListenerResponse"/> object to pass to this endpoint.</param>
+		public JsonEndpoint(HttpListenerRequest request, HttpListenerResponse response) : base(request, response) { }
+
+		/// <summary>
+		/// Invokes this <see cref="JsonEndpoint"/>.
+		/// </summary>
+		/// <remarks>
+		/// This function cannot be overridden.
+		/// </remarks>
+		public sealed override void Invoke()
+		{
+			// Read the inputstream of the request and convert it to a JObject
+			//var content = JObject.Parse(new StreamReader(Request.InputStream, Request.ContentEncoding).ReadToEnd());
+			var content = new JObject();
+			var parameters = SplitQuery(Request.Url.Query);
+			switch (Request.HttpMethod)
+			{
+				case "GET": GET(content, parameters); break;
+				case "POST": POST(content, parameters); break;
+				case "DELETE": DELETE(content, parameters); break;
+				case "PATCH": PATCH(content, parameters); break;
+				default: Server.SendError(Response, HttpStatusCode.NotImplemented); break;
+			}
+		}
+
+		/// <summary>
+		/// Endpoint for the http GET method. This must be implemented.
+		/// </summary>
+		/// <param name="json">The json sent by the client.</param>
+		/// <param name="parameters">A dictionary containing any url parameters.</param>
+		protected abstract void GET(JObject json, Dictionary<string, string> parameters);
+		/// <summary>
+		/// Endpoint for the http POST method.
+		/// </summary>
+		/// <param name="json">The json sent by the client.</param>
+		/// <param name="parameters">A dictionary containing any url parameters.</param>
+		protected virtual void POST(JObject json, Dictionary<string, string> parameters) => Server.SendError(Response, HttpStatusCode.NotImplemented);
+		/// <summary>
+		/// Endpoint for the http DELETE method.
+		/// </summary>
+		/// <param name="json">The json sent by the client.</param>
+		/// <param name="parameters">A dictionary containing any url parameters.</param>
+		protected virtual void DELETE(JObject json, Dictionary<string, string> parameters) => Server.SendError(Response, HttpStatusCode.NotImplemented);
+		/// <summary>
+		/// Endpoint for the http PATCH method.
+		/// </summary>
+		/// <param name="json">The json sent by the client.</param>
+		/// <param name="parameters">A dictionary containing any url parameters.</param>
+		protected virtual void PATCH(JObject json, Dictionary<string, string> parameters) => Server.SendError(Response, HttpStatusCode.NotImplemented);
+	}
+}
