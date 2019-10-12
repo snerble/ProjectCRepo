b9cd7e11bd0720f0995a9d61fee29e455777897b
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-10-12 23:16:39 +0200

    Added proof of concept endpoint

---
+++ Web API/Reworked Web API/HTTP/Endpoints/Example/EndpointTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace API.HTTP.Endpoints
+{
+	/// <summary>
+	/// Test subclass of <see cref="JsonEndpoint"/>. Will be removed later.
+	/// </summary>
+	[EndpointUrl("/test.json")]
+	public sealed class EndpointTest : JsonEndpoint
+	{
+		public EndpointTest(HttpListenerRequest request, HttpListenerResponse response) : base(request, response) { }
+
+		protected override void GET(JObject json, Dictionary<string, string> parameters)
+		{
+			if (parameters.Count == 0)
+			{
+				Server.SendError(Response, HttpStatusCode.OK);
+				return;
+			}
+			string outtext = "<html>";
+			int max = parameters.Max(x => x.Key.Length);
+			foreach (var item in parameters)
+			{
+				outtext += $"{item.Key}:{new string(' ', max - item.Key.Length)}   {item.Value}<br>";
+			}
+			Server.SendText(Response, outtext + "</html>");
+		}
+	}
+}
