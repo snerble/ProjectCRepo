da488418e8821ff51de7ba76b0419a026eebb57e
Amy <43269323+amynowe@users.noreply.github.com>
2019-10-13 21:59:50 +0200

    Loginbutton opent de takenlijst
    Loginbutton opent als het goed is de takenlijst bij correcte inloggegevens: username: admin // password: admin)
    LET OP: dit moet nog getest worden omdat ik android studio niet correct opgezet kreeg

--- Mobile App/Alldone/app/src/main/java/com/example/alldone/MainActivity.java
+++ Mobile App/Alldone/app/src/main/java/com/example/alldone/MainActivity.java
@@ -6,11 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+    EditText username = (EditText)findViewById(R.id.user);
+    EditText password = (EditText)findViewById(R.id.password);
+    Button login = (Button)findViewById(R.id.buttonLogin);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        login.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                Toast.makeText(getApplicationContext(), "Doorsturen...",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, Takenlijst.class));
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Verkeerde gebruikersnaam en/of wachtwoord",Toast.LENGTH_SHORT).show();
+            }
+         }
+      });
+
+      login.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            finish();
+         }
+      });	
     }
 }
