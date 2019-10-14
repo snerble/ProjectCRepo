3285fb2922b464452d2aab097c4b3ce2a1250b65
Kirbman10 <38724165+Kirbman10@users.noreply.github.com>
2019-10-14 13:50:29 +0200

    fixed login code

--- Mobile App/Alldone/app/src/main/java/com/example/alldone/MainActivity.java
+++ Mobile App/Alldone/app/src/main/java/com/example/alldone/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.alldone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,15 +12,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText username = (EditText)findViewById(R.id.user);
-    EditText password = (EditText)findViewById(R.id.password);
-    Button login = (Button)findViewById(R.id.buttonLogin);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        final EditText username = (EditText)findViewById(R.id.user);
+        final EditText password = (EditText)findViewById(R.id.password);
+        final Button login = (Button)findViewById(R.id.buttonLogin);
+
         login.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
@@ -32,12 +34,5 @@ public class MainActivity extends AppCompatActivity {
             }
          }
       });
-
-      login.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-            finish();
-         }
-      });	
     }
 }
