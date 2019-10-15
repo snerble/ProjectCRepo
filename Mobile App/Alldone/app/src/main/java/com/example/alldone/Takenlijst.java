1be2b075f1c345c62013fa5b7d46ae58b07d371f
Kirbman10 <38724165+Kirbman10@users.noreply.github.com>
2019-10-15 16:43:10 +0200

    added recyclerview functionality

--- Mobile App/Alldone/app/src/main/java/com/example/alldone/Takenlijst.java
+++ Mobile App/Alldone/app/src/main/java/com/example/alldone/Takenlijst.java
@@ -1,14 +1,45 @@
 package com.example.alldone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Takenlijst extends AppCompatActivity {
+
+    private static final String TAG = "Takenlijst";
+
+    private ArrayList<String> titleList = new ArrayList<>();
+    private ArrayList<String> usersList = new ArrayList<>();
+    private ArrayList<String> priorityList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_takenlijst);
+
+        initArrays();
+    }
+
+    private void initArrays(){
+        titleList.add("Koffiebonen bijvullen");
+        usersList.add("Open voor inschrijving");
+        priorityList.add("");
+        titleList.add("Bureau repareren");
+        usersList.add("Amy, Daphne");
+        priorityList.add("!!");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        TakenLijstViewAdapter adapter = new TakenLijstViewAdapter(titleList, usersList, priorityList, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
