5bd270faf8cbf9a7a644ee9892a168a0d93668f2
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-10-18 20:08:20 +0200

    Changed AllowMultiple to false

--- Web API/Reworked Web API/HTTP/Endpoints/EndpointUrl.cs
+++ Web API/Reworked Web API/HTTP/Endpoints/EndpointUrl.cs
@@ -5,7 +5,7 @@ namespace API.HTTP.Endpoints
 	/// <summary>
 	/// A custom attribute class meant to decorate <see cref="Endpoint"/> subclasses and specify their target url.
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
 	public sealed class EndpointUrl : Attribute
 	{
 		/// <summary>
