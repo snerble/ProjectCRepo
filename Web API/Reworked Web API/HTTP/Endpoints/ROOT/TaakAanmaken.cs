ce0a2acb588739b951347bf2e58da079bac9cf14
Cindy Zheng <0950001@hr.nl>
2019-10-24 17:42:17 +0200

    Endpoint taakaanmaken.cs gemaakt + TaakAanmaken.cshtml

---
+++ Web API/Reworked Web API/HTTP/Endpoints/ROOT/TaakAanmaken.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using RazorEngine;
+using RazorEngine.Templating;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace API.HTTP.Endpoints
+{
+    [EndpointUrl("/taakaanmaken")]
+    public sealed class HTMLTaakAanmaken : HTMLEndpoint
+    {
+        //Absolute pad naar html folder, HTMLServer.cs voor meer info
+        public string HtmlDir => Path.GetFullPath(Path.Combine(
+            Directory.GetCurrentDirectory(),
+            Program.Config["serverSettings"]["htmlSourceDir"].ToObject<string>()
+        ));
+        
+        public HTMLTaakAanmaken(HttpListenerRequest request, HttpListenerResponse response) : base(request, response) { }
+
+        public override void GET(Dictionary<string, string> parameters)
+            => Server.SendText(Response, Templates.RunTemplate(GetUrl<HTMLTaakAanmaken>() + ".cshtml", Request, parameters));
+
+        public override void POST(Dictionary<string, string> parameters)
+        {
+            Program.Log.Debug("Received post request.");
+
+            // voeg cookie toe
+            // cookie is false
+            // if button is clicked
+            // cookie is true
+            // laat nieuwe page zien
+            
+
+        }
+    }
+
+   
+}
