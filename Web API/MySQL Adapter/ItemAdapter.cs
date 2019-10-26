fe3e4a58c9bc9172f96939e7fd0a6f1aef849d07
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-10-26 14:11:32 +0200

    Began work on MySQL database modeling framework

---
+++ Web API/MySQL Adapter/ItemAdapter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MySQL.Modeling
+{
+	/// <summary>
+	/// Abstract class for modeling a MySQL table item into an object.
+	/// </summary>
+	public abstract class ItemAdapter
+	{
+	}
+}
