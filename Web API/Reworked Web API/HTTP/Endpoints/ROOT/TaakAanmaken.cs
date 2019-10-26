51c84df8a1c112ac71edad7dffdcbcce296dd7f4
Cindy Zheng <0950001@hr.nl>
2019-10-26 23:03:53 +0200

    Cookie functions in taakaanmaken.cshtml

--- Web API/Reworked Web API/HTTP/Endpoints/ROOT/TaakAanmaken.cs
+++ Web API/Reworked Web API/HTTP/Endpoints/ROOT/TaakAanmaken.cs
@@ -1,9 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
-using RazorEngine;
-using RazorEngine.Templating;
-using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Net;
 
 namespace API.HTTP.Endpoints
@@ -11,26 +6,17 @@ namespace API.HTTP.Endpoints
     [EndpointUrl("/taakaanmaken")]
     public sealed class HTMLTaakAanmaken : HTMLEndpoint
     {
-        //Absolute pad naar html folder, HTMLServer.cs voor meer info
-        public string HtmlDir => Path.GetFullPath(Path.Combine(
-            Directory.GetCurrentDirectory(),
-            Program.Config["serverSettings"]["htmlSourceDir"].ToObject<string>()
-        ));
-        
         public HTMLTaakAanmaken(HttpListenerRequest request, HttpListenerResponse response) : base(request, response) { }
 
         public override void GET(Dictionary<string, string> parameters)
+            //Templates worden naar client gestuurd, url wordt van endpoint gehaald
             => Server.SendText(Response, Templates.RunTemplate(GetUrl<HTMLTaakAanmaken>() + ".cshtml", Request, parameters));
 
         public override void POST(Dictionary<string, string> parameters)
         {
             Program.Log.Debug("Received post request.");
 
-            // voeg cookie toe
-            // cookie is false
-            // if button is clicked
-            // cookie is true
-            // laat nieuwe page zien
+           
             
 
         }
