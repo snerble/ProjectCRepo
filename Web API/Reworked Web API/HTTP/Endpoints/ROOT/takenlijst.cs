a09475ce4916cfa872943c9ab7cf15d641dde971
Cindy Zheng <0950001@hr.nl>
2019-10-28 02:19:33 +0100

    Cookie can be deleted (ish), takenlijst endpoint nog niet af

---
+++ Web API/Reworked Web API/HTTP/Endpoints/ROOT/takenlijst.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace API.HTTP.Endpoints
+{
+    [EndpointUrl("/takenlijst")]
+    public sealed class HTMLtakenlijst : HTMLEndpoint
+    {
+        public HTMLtakenlijst(HttpListenerRequest request, HttpListenerResponse response) : base(request, response) { }
+
+        public override void GET(Dictionary<string, string> parameters)
+            //Templates worden naar client gestuurd, url wordt van endpoint gehaald
+            => Server.SendText(Response, Templates.RunTemplate(GetUrl<HTMLtakenlijst>() + ".cshtml", Request, parameters));
+
+        public override void POST(Dictionary<string, string> parameters)
+        {
+            Program.Log.Debug("Received post request.");
+
+
+
+
+        }
+    }
+
+
+}
