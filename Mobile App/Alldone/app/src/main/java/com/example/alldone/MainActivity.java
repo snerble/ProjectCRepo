f6819b2cabbb45746925e20255d48d43f1a4bd13
Kirbman10 <38724165+Kirbman10@users.noreply.github.com>
2019-11-15 15:37:19 +0100

    start login shizzle

--- Mobile App/Alldone/app/src/main/java/com/example/alldone/MainActivity.java
+++ Mobile App/Alldone/app/src/main/java/com/example/alldone/MainActivity.java
@@ -2,10 +2,23 @@ package com.example.alldone;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+            /*if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
                 Toast.makeText(getApplicationContext(), "Je bent ingelogd",Toast.LENGTH_SHORT).show();
                 Intent intent0 = new Intent(getApplicationContext(), Takenlijst.class);
                 intent0.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -33,8 +46,55 @@ public class MainActivity extends AppCompatActivity {
             }
             else {
                 Toast.makeText(getApplicationContext(), "Verkeerde gebruikersnaam en/of wachtwoord",Toast.LENGTH_SHORT).show();
-            }
+            }*/
+            System.out.println("Yo wassup");
+            Thread e = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    NetworkingShit();
+                }
+            });
+            e.start();
          }
       });
+    }
+
+    public void NetworkingShit() {
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL("http://145.137.53.159/testjson");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setDoInput(true);
+//            urlConnection.setDoOutput(true);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+            System.out.println("Connected.");
+
+//            DataOutputStream os = new DataOutputStream(urlConnection.getOutputStream());
+//            os.flush();
+//            os.close();
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(
+                            new DataInputStream(
+                                    urlConnection.getInputStream()
+                            )
+                    )
+            );
+            StringBuilder sb = new StringBuilder();
+            String s = null;
+            while ((s = br.readLine()) != null)
+                sb.append(s);
+            br.close();
+            System.out.println(sb.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null)
+                urlConnection.disconnect();
+        }
     }
 }
