4d99d84cc02dd71d2d64842f39f466e171f2fbb5
Cindy Zheng <0950001@hr.nl>
2019-11-15 14:15:17 +0100

    uploadtaak.cs aangemaakt, en database spul getest

--- Web API/Reworked Web API/HTTP/Endpoints/ROOT/TaakAanmaken.cs
+++ Web API/Reworked Web API/HTTP/Endpoints/ROOT/TaakAanmaken.cs
@@ -1,18 +1,57 @@
-﻿using System.Collections.Generic;
+﻿using API.Database;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 
 namespace API.HTTP.Endpoints
 {
+
     [EndpointUrl("/taakaanmaken")]
     public sealed class HTMLTaakAanmaken : HTMLEndpoint
     {
         public override void GET(Dictionary<string, string> parameters)
+        {
+            var user = Program.Database.Select<User>().LastOrDefault();
+            var task = Program.Database.Select<Task>().LastOrDefault();
+
             //Templates worden naar client gestuurd, url wordt van endpoint gehaald
-            => Server.SendText(Response, Templates.RunTemplate(GetUrl<HTMLTaakAanmaken>() + ".cshtml", Request, parameters));
+            Server.SendText(Response,
+                Templates.RunTemplate(
+                    GetUrl<HTMLTaakAanmaken>() + ".cshtml",
+                    Request,
+                    parameters,
+                    new
+                    {
+                        User = user,
+                        Task = task
+                    }
+                )
+            );
+        }
 
         public override void POST(Dictionary<string, string> parameters)
         {
-            Program.Log.Debug("Received post request.");
+
+            var users = Program.Database.Select<User>().ToList();
+            foreach (var user in users)
+                Program.Log.Info(user);
+
+            var tasks = Program.Database.Select<Task>().ToList();
+            foreach (var task in tasks)
+                Program.Log.Info(task);
+
+            Program.Database.Insert(new User()
+            {
+                Username = "heheh",
+                Password = "1"
+            });
+
+            Program.Database.Insert(new Task() 
+            { 
+                Title = "pls"
+            });
+
+            //Program.Log.Debug("Received post request.");
 
             //Response.Redirect("");
             //Server.SendError(Response, HttpStatusCode.Redirect);
---
+++ Web API/Reworked Web API/HTTP/Endpoints/ROOT/uploadtaak.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.HTTP.Endpoints
+{
+    [EndpointUrl("/uploadtaak")]
+    public sealed class HTMLuploadtaak : HTMLEndpoint
+    {
+        public override void GET(Dictionary<string, string> parameters)
+        {
+            
+        }
+    }
+}
