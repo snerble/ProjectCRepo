b4b78b64d9e765e5cdd84010e239cbe2a718782d
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-11-20 18:24:37 +0100

    Added tools for JSON endpoints

--- Web API/Reworked Web API/HTTP/Endpoints/JsonEndpoint.cs
+++ Web API/Reworked Web API/HTTP/Endpoints/JsonEndpoint.cs
@@ -3,6 +3,7 @@ using System.Net;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace API.HTTP.Endpoints
 {
@@ -74,5 +75,71 @@ namespace API.HTTP.Endpoints
 		/// <param name="json">The json sent by the client.</param>
 		/// <param name="parameters">A dictionary containing all url parameters.</param>
 		public virtual void PATCH(JObject json, Dictionary<string, string> parameters) => Server.SendError(HttpStatusCode.NotImplemented);
+
+		/// <summary>
+		/// Validates an array of parameters by checking if they are present and match the predicate. A response is
+		/// immediately sent to the client if this function returns false.
+		/// </summary>
+		/// <param name="json">The json from which to get the values.</param>
+		/// <param name="predicates">An array of tuples containing the parameter name and a predicate.</param>
+		/// <returns>True if the validation succeeded. False otherwise.</returns>
+		protected bool ValidateParams(JObject json, params (string, Func<JToken, bool>)[] predicates)
+			=> ValidateParams(json, ValidationMode.Required, predicates);
+		/// <summary>
+		/// Validates an array of parameters by checking if they are present and match the predicate. A response is
+		/// immediately sent to the client if this function returns false.
+		/// </summary>
+		/// <param name="json">The json from which to get the values.</param>
+		/// <param name="mode">The type of validation checking to use.</param>
+		/// <param name="predicates">An array of tuples containing the parameter name and a predicate.</param>
+		/// <returns>True if the validation succeeded. False otherwise.</returns>
+		protected bool ValidateParams(JObject json, ValidationMode mode, params (string, Func<JToken, bool>)[] predicates)
+		{
+			// Get all missing parameters
+			var missing = new List<string>();
+			foreach (var (name, predicate) in predicates)
+				if (!json.ContainsKey(name))
+					missing.Add(name);
+
+			// Check predicates for every value that isn't missing
+			var invalid = new List<string>();
+			foreach (var (name, predicate) in predicates.Where(x => !missing.Contains(x.Item1)))
+				if (!predicate(json[name]))
+					invalid.Add(name);
+
+			var outJson = new JObject();
+			if (mode == ValidationMode.Required && missing.Any()) outJson.Add("missing", new JArray(missing));
+			if (mode == ValidationMode.Options && missing.Count == predicates.Count())
+				outJson.Add("missing", new JObject() { "options", new JArray(missing) });
+
+			// Get every param where their predicate returns false
+			var failed = invalid.Where(x => predicates.First(y => x == y.Item1).Item2(json.GetValue(x)));
+			if (failed.Any()) outJson.Add("invalid", new JArray(failed));
+
+			// Return true if the validation didnt encounter errors
+			if (outJson.Count == 0) return true;
+			// Send the json if there were errors
+			Server.SendJSON(outJson, HttpStatusCode.UnprocessableEntity);
+			return false;
+		}
+
+		/// <summary>
+		/// Specifies how the parameter validation will behave.
+		/// </summary>
+		protected enum ValidationMode
+		{
+			/// <summary>
+			/// All specified parameters are required and must pass validation.
+			/// </summary>
+			Required,
+			/// <summary>
+			/// At least one of the specified parameters are required and all given parameters must pass validation.
+			/// </summary>
+			Options,
+			/// <summary>
+			/// None of the parameters are required, but all given parameters must pass validation.
+			/// </summary>
+			Optional
+		}
 	}
 }
