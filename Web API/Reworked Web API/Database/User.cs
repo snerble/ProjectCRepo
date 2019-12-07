ec5263bb5456a574ade677ba5e6f215be8399b02
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-12-07 14:52:11 +0100

    Added password hashing function and renamed the Token column to Created

--- Web API/Reworked Web API/Database/User.cs
+++ Web API/Reworked Web API/Database/User.cs
@@ -1,6 +1,8 @@
 ﻿using MySql.Data.MySqlClient;
 using MySQL.Modeling;
 using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace API.Database
 {
@@ -35,7 +37,21 @@ namespace API.Database
 		public int? Id { get; set; }
 		public string Username { get; set; }
 		public string Password { get; set; }
-		public long Token { get; set; } = DateTimeOffset.Now.ToUnixTimeSeconds();
+		public long Created { get; set; } = DateTimeOffset.Now.ToUnixTimeSeconds();
 		public AccessLevel AccessLevel { get; set; } = AccessLevel.User;
+
+		/// <summary>
+		/// Returns a salted <see cref="SHA256"/> password hash using this <see cref="User"/>'s username and password.
+		/// </summary>
+		/// <returns></returns>
+		public string GetSHA256PasswordHash()
+		{
+			// Create a sha instance
+			using var sha256 = SHA256.Create();
+			// Compute the hash
+			var passwordHash = sha256.ComputeHash(Encoding.UTF8.GetBytes(Username + "#:#" + Password));
+			// Convert the hash bytes back to a string and return it
+			return Encoding.UTF8.GetString(passwordHash);
+		}
 	}
 }
