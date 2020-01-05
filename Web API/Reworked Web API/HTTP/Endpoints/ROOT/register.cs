b94aa25d18d090acaa5842e083dc9e278033c6bb
Spaalone_Babuguuscooties <0965749@hr.nl>
2020-01-05 19:01:12 +0100

    Changed namespace

--- Web API/Reworked Web API/HTTP/Endpoints/ROOT/register.cs
+++ Web API/Reworked Web API/HTTP/Endpoints/ROOT/register.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 
-namespace API.HTTP.Endpoints.ROOT
+namespace API.HTTP.Endpoints
 {
 	[EndpointUrl("/register")]
     public class Register : HTMLEndpoint
