4d1ba733a1f4b2881a29bba305d0dd42a3dc598b
Amy <0963427@hr.nl>
2020-01-06 13:40:32 +0100

    idfk2

--- Web API/Reworked Web API/HTTP/Endpoints/ROOT/register.cs
+++ Web API/Reworked Web API/HTTP/Endpoints/ROOT/register.cs
@@ -1,43 +1,51 @@
-﻿using System;
+﻿using API.Database;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Text;
-using API.Database;
-using Newtonsoft.Json.Linq;
 
-namespace API.HTTP.Endpoints
+namespace API.HTTP.Endpoints.ROOT
 {
-    [EndpointUrl("/register")]
-    public sealed class register : JsonEndpoint
+	[EndpointUrl("/register")]
+    public class Register : HTMLEndpoint
     {
-        public override void GET(JObject json, Dictionary<string, string> parameters)
-            => Server.SendError(HttpStatusCode.NotImplemented);
-        public override void POST(JObject json, Dictionary<string, string> parameters)
+        public override void GET(Dictionary<string, string> parameters)
+            => Server.SendFile(Program.Config.HTMLSourceDir + "/register.html");
+
+        public override void POST(Dictionary<string, string> parameters)
         {
-            if (!json.TryGetValue("username", out JToken usernameToken))
-            {
-                Server.SendError(HttpStatusCode.BadRequest);
-                return;
-            }
-            if (!json.TryGetValue("password", out JToken passwordToken))
-            {
-                Server.SendError(HttpStatusCode.BadRequest);
-                return;
-            }
+			// Validate the presence of all required parameters
+			if (!ValidateParams(parameters,
+					("username", x => x.Any()), // Username may not be empty
+					("password", x => x.Length == 128))) // Password must be 128 characters long (thus must be hashed with sha512)
+			{
+				// Send a 400 Bad Request if any required parameters are missing
+				Server.SendError(HttpStatusCode.BadRequest);
+				return;
+			}
 
-            string username = usernameToken.Value<string>();
-            string password = passwordToken.Value<string>();
+			// Get all required parameters
+			var username = parameters["username"];
+			var password = parameters["password"];
 
-            var user = Program.Database.Select<User>($"username = '{username}' AND password = '{password}'").FirstOrDefault();
+			// Check if the user does not already exist
+			if (Database.Select<User>($"`username` = '{username}'").Any())
+			{
+				// Send 409 Conflict status code to indicate a duplicate
+				Server.SendError(HttpStatusCode.Conflict);
+				return;
+			}
 
-            if(user == null)
-            {
-                var newUser = new User();
-                newUser.Username = username;
-                newUser.Password = password;
-                Program.Database.Insert<User>(newUser);
-            }
+			// Create the new user object
+			var user = new User() { Username = username, Password = password };
+			// Salt and hash it's password
+			user.Password = user.GetPasswordHash();
+
+			// Upload the new user to the database
+			Database.Insert(user);
+
+			// Redirect the client to the login
+			Response.Redirect(GetUrl<JLogin>());
+			Server.SendError(HttpStatusCode.Redirect);
         }
     }
 }
