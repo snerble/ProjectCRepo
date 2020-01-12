45d801cb3c005fa8a466ec84bfc2a300415dd881
Amy <0963427@hr.nl>
2020-01-12 17:18:04 +0100

    #3 Removed unused (commented) code

--- Mobile App/Alldone/app/src/main/java/com/example/alldone/MaakTaak.java
+++ Mobile App/Alldone/app/src/main/java/com/example/alldone/MaakTaak.java
@@ -17,19 +17,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -100,7 +89,6 @@ public class MaakTaak extends AppCompatActivity implements NavigationView.OnNavi
 
         class SendPostReqAsyncTask extends AsyncTask<Void, Void, Response> {
         // Using AsyncTask to execute heavier tasks in the background on a dedicated thread
-        // --> the app runs on a single thread, thus executing InsertData() which takes time to get a responsive could make the app non-responsive.
 
             @Override
             protected Response doInBackground(Void... params) {
@@ -136,7 +124,6 @@ public class MaakTaak extends AppCompatActivity implements NavigationView.OnNavi
         }
 
         SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
-
         sendPostReqAsyncTask.execute();
     }
 
